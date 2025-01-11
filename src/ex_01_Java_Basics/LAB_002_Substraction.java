8bf22e91f9af71209954d96c9b90251e6f0fd8ae
Anupam Rai <raianupam73@gmail.com>
2025-01-11 09:04:00 +0530

    5th Jan

---
+++ src/ex_01_Java_Basics/LAB_002_Substraction.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class LAB_002_Substraction {
+    public static void main(String[] args) {
+        System.out.println(120/4);
+    }
+}
---
+++ src/ex_01_Java_Basics/LAB_002_Sum.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class LAB_002_Sum {
+
+    public static void main(String[] args) {
+        System.out.println(2+2);
+    }
+}
