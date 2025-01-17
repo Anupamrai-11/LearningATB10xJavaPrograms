67a75d4d02e166e1314ae6375b83ac8ba17f2be0
Anupam Rai <raianupam73@gmail.com>
2025-01-17 07:40:15 +0530

    17th Jan

---
+++ src/ex_02_Java_Basics/LAB_011_Printf_Task.java
@@ -0,0 +1,26 @@
+package ex_02_Java_Basics;
+
+public class LAB_011_Printf_Task {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d",a);
+        System.out.println();
+        System.out.printf("%d",a*2);
+        System.out.println();
+        System.out.printf("%d",a*3);
+        System.out.println();
+        System.out.printf("%d",a*4);
+        System.out.println();
+        System.out.printf("%d",a*5);
+        System.out.println();
+        System.out.printf("%d",a*6);
+        System.out.println();
+        System.out.printf("%d",a*7);
+        System.out.println();
+        System.out.printf("%d",a*8);
+        System.out.println();
+        System.out.printf("%d",a*9);
+        System.out.println();
+        System.out.printf("%d",a*10);
+    }
+}
