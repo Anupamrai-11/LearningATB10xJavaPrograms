d4c9ebbe92ada32c4abcd4e679d85d6bdd9bc4b7
Anupam Rai <raianupam73@gmail.com>
2025-01-26 12:22:22 +0530

    26 Jan 2

---
+++ src/ex_04_Operators/LAB_037_Ternary_Operator1.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+public class LAB_037_Ternary_Operator1 {
+    public static void main(String[] args) {
+        // result = condition ? expression1 : expression2
+
+        int i = 16;
+        String s1 = i>=18 ? "No,You Can't":"Yes,You Can";
+        System.out.println(s1);
+
+        int j =16;
+        String s2 = j<=16 ? "A":"B";
+        System.out.println(s2);
+    }
+}
---
+++ src/ex_04_Operators/LAB_038_Ternary_Operator2.java
@@ -0,0 +1,10 @@
+package ex_04_Operators;
+
+public class LAB_038_Ternary_Operator2 {
+
+    public static void main(String[] args) {
+        int i = -89;
+        String s1= i>=0 ? "Positive":"Negative";
+        System.out.println(s1);
+    }
+}
