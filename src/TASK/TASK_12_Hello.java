880006102575a5d2327099a2210657a1486ef56c
Anupam Rai <raianupam73@gmail.com>
2025-02-08 12:53:37 +0530

    08 Feb part 2

---
+++ src/TASK/TASK_12_Hello.java
@@ -0,0 +1,9 @@
+package TASK;
+
+public class TASK_12_Hello {
+    public static void main(String[] args) {
+        for (int i=1;i<=5 ; i++){
+            System.out.println("HELLO");
+        }
+    }
+}
---
+++ src/TASK/TASK_14_Multiplication_Table_Of_Five.java
@@ -0,0 +1,10 @@
+package TASK;
+
+public class TASK_14_Multiplication_Table_Of_Five {
+    public static void main(String[] args){
+            int n=5;
+            for (int i=1;i<=10;i++){
+                System.out.println(n*i);
+            }
+    }
+}
